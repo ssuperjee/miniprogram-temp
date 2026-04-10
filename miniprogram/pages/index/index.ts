98098f6d9c7ca70bc01532ec444979ef7807e5a6
ssuperjee <553893374@qq.com>
2026-04-10 14:50:14 +0800

    引入vant

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,13 +1,20 @@
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
 
 Page({
-  /**
-   * 页面初始数据
-   */
   data: {
-    modalSubmitting: false
+    // 页面数据
   },
 
   onLoad() {
-   
+    // 页面加载时的逻辑
   },
+  
+  showModal() { 
+    Dialog.alert({
+      title: '提示',
+      message: '这是一个模态框',
+    }).then(() => {
+      // on close
+    });
+  }
 });
